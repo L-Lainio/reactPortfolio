3af9796c92e13cd80f52afbe16f9d222e6e52235
L-Lainio <arollainio@gmail.com>
2024-06-07 11:55:55 -0400

    made changes to app.jsx file

--- Main/src/App.jsx
+++ Main/src/App.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
-import Experience from './components/Experience';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
