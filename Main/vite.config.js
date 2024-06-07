56273f057cd1af9e5f6dff80ed96d2c97f7a3a01
L-Lainio <arollainio@gmail.com>
2024-06-07 01:13:41 -0400

    updated defineConfig plugin

--- Main/vite.config.js
+++ Main/vite.config.js
@@ -3,9 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
 })
+
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     open: true
+//   }
+// })
