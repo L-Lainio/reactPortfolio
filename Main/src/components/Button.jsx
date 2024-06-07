6f233716029afddfc055770c7dc1944a3e05a511
L-Lainio <arollainio@gmail.com>
2024-06-06 23:35:02 -0400

    updated and revised code from bucket to button, contact, and skills.jsx

---
+++ Main/src/components/Button.jsx
@@ -0,0 +1,22 @@
+// other emblem inport should i use?if decided add it to the import list
+//  import ChevronsRight from './Icons/ChevronsRight';
+
+const Button = ({ children }) => {
+    return (
+        <button
+            id='outline-btn'
+            type='submit'
+            className='group'
+        >
+            {children}
+            <span className='group-hover:text-emerald-400 dark:group-hover:text-teal-200 group-hover:ml-1 duration-300'>
+                {/* <ChevronsRight
+                    size={15}
+                    className='ml-2'
+                /> */}
+            </span>
+        </button>
+    );
+};
+
+export default Button;
--- Main/src/components/Contact.jsx
+++ Main/src/components/Contact.jsx
@@ -1,76 +1,184 @@
 import { useState } from 'react';
-import BucketForm from './BucketForm';
-import Bucket from './Bucket';
+// once added for motion add import here
+// import { motion } from 'framer-motion';
+// import GitHub from './Icons/GitHub';
+import Skills from './About/Skills';
+import  Button from './Button';
 
-function BucketList() {
-  const [bucket, setBucket] = useState([]);
+const Contact = () => {
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
+  const formId = import.meta.env.VITE_GETFORM_ID;
+  const formUrl = 'https://getform.io/f/' + formId;
 
-  // Function to add a bucket list item
-  const addBucketItem = (item) => {
-    console.log(
-      'File: BucketList.js ~ line 10 ~ addBucketItem ~ item',
-      item
-    );
-    // Check to see if the item text is empty
-    if (!item.text) {
-      return;
-    }
-
-    // Add the new bucket list item to the existing array of objects
-    const newBucket = [item, ...bucket];
-    console.log(newBucket);
-
-    // Call setBucket to update state with our new set of bucket list items
-    setBucket(newBucket);
-  };
-
-  // Function to mark bucket list item as complete
-  const completeBucketItem = (id) => {
-    // If the ID passed to this function matches the ID of the item that was clicked, mark it as complete
-    let updatedBucket = bucket.map((item) => {
-      if (item.id === id) {
-        item.isComplete = !item.isComplete;
+  // handles form validation
+  function handleChange(e) {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      // error message for invalid email
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
       }
-      return item;
-    });
-
-    console.log(updatedBucket);
-    setBucket(updatedBucket);
-  };
-
-  // Function to remove bucket list item and update state
-  const removeBucketItem = (id) => {
-    const updatedBucket = [...bucket].filter((item) => item.id !== id);
-
-    setBucket(updatedBucket);
-  };
-
-  // Function to edit the bucket list item
-  const editBucketItem = (itemId, newValue) => {
-    // Make sure that the value isn't empty
-    if (!newValue.text) {
-      return;
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${capitalizeFirstLetter(e.target.name)} is required.`);
+      } else {
+        setErrorMessage('');
+      }
     }
-
-    // We use the "prev" argument provided with the useState hook to map through our list of items
-    // We then check to see if the item ID matches the if of the item that was clicked and if so we set it to a new value
-    setBucket((prev) =>
-      prev.map((item) => (item.id === itemId ? newValue : item))
-    );
-  };
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
 
   return (
-    <div>
-      <h1>What is on your bucket list?</h1>
-      <BucketForm onSubmit={addBucketItem} />
-      <Bucket
-        bucket={bucket}
-        completeBucketItem={completeBucketItem}
-        removeBucketItem={removeBucketItem}
-        editBucketItem={editBucketItem}
-      ></Bucket>
-    </div>
-  );
-}
+    <section
+      id='contact'
+      className='container'
+    >
+      <motion.h2
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4, delay: 0.5 }}
+        viewport={{ once: true }}
+      >
+        Contact
+      </motion.h2>
+      <motion.div
+        className='max-w-screen-lg mx-auto w-full grid grid-cols-1 md:grid-cols-2 gap-4'
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4, delay: 0.75 }}
+        viewport={{ once: true }}
+      >
+        <div>
+          <p>
+            Please feel free to reach out with any questions or opportunities,
+            and I'll do my best to respond as soon as possible. I look forward
+            to hearing from you!
+          </p>
 
-export default BucketList;
+          <div className='max-w-[90%] w-full mx-auto py-8 grid grid-cols-2 gap-2 sm:gap-4'>
+            <div className='inline-flex items-center'>
+              <span className='text-emerald-400 dark:text-teal-200'>
+                <MapPin />
+              </span>
+              &nbsp;Location
+            </div>
+            <div className='inline-flex justify-end items-center'>
+              : Lake City, FL
+            </div>
+            <div className='inline-flex items-center'>
+              <span className='text-emerald-400 dark:text-teal-200'>
+                <AtSign />
+              </span>
+              &nbsp;E-mail
+            </div>
+            <div className='inline-flex justify-end items-center'>
+              :&nbsp;
+              <a
+                href='mailto:arollainio@gmail.com'
+                className='text-emerald-400 dark:text-teal-200 hover:text-slate-500 dark:hover:text-blue-400'
+                aria-label='arollainio@gmail.com'
+              >
+                arollainio@gmail.com
+              </a>
+            </div>
+            <div className='inline-flex items-center'>
+              <span className='text-emerald-400 dark:text-teal-200'>
+                <GitHub />
+              </span>
+              &nbsp;GitHub
+            </div>
+            <div className='inline-flex justify-end items-center'>
+              :&nbsp;
+              <a
+                href='https://github.com/L-Lainio'
+                target='_blank'
+                className='text-emerald-400 dark:text-teal-200 hover:text-slate-500 dark:hover:text-blue-400'
+                aria-label='GitHub: @L-Lainio'
+              >
+                @L-Lainio
+              </a>
+            </div>
+            <div className='inline-flex items-center'>
+              <span className='text-emerald-400 dark:text-teal-200'>
+                <Globe />
+              </span>
+              &nbsp;Languages
+            </div>
+            <div className='inline-flex justify-end items-center'>
+              : English, Korean
+            </div>
+          </div>
+        </div>
+
+        {/* CONTACT FORM */}
+        <div>
+          <form
+            method='POST'
+            action={formUrl}
+            id='contact-form'
+            className='flex flex-col justify-center max-w-[600px] mx-auto w-full'
+          >
+            <div class='w-full mx-auto'>
+              <label for='name'>
+                Name:
+                <input
+                  type='text'
+                  id='name'
+                  name='name'
+                  aria-labelledby='name'
+                  defaultValue={name}
+                  onBlur={handleChange}
+                />
+              </label>
+
+              <label for='email'>
+                E-mail:
+                <input
+                  type='email'
+                  id='email'
+                  name='email'
+                  aria-labelledby='email'
+                  defaultValue={email}
+                  onBlur={handleChange}
+                />
+              </label>
+
+              <label for='message'>
+                Message:
+                <textarea
+                  name='message'
+                  id='message'
+                  aria-labelledby='message'
+                  defaultValue={message}
+                  onBlur={handleChange}
+                  rows='4'
+                />
+              </label>
+
+              {errorMessage && (
+                <div>
+                  <p className='text-slate-500 dark:text-blue-400 font-["Source_Code_pro"] font-medium tracking-tight text-xs'>
+                    {errorMessage}
+                  </p>
+                </div>
+              )}
+              <Button>Submit</Button>
+            </div>
+          </form>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Contact;
