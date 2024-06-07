1272b233cf9f04ddbe68224ee5cd623b42c02e81
L-Lainio <arollainio@gmail.com>
2024-06-07 01:19:47 -0400

    import tailwindcss

---
+++ Main/tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'main-bg': "url('/src/assets/bg.png')",
+            }
+        },
+    },
+    plugins: [],
+    darkMode: "class"
+}
