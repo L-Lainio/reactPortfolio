f0aec3159c5e3861699adfe8bdc397db6a3d8ed2
L-Lainio <arollainio@gmail.com>
2024-06-08 03:21:13 -0400

    cleaning up duplications still

--- Main/src/App.jsx
+++ Main/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react';
-
-import Home from './components/Home';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import React from 'react';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import Home from './components/Home';
+// import About from './components/About';
+// import Portfolio from './components/Portfolio';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
+// import React from 'react';
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
   const [darkMode, setDarkMode] = useState(false);
 
   const toggleDarkMode = () => {
@@ -16,26 +18,38 @@ function App() {
   };
 
   return (
-    <main className={`${darkMode ? 'dark' : 'light'}`}>
+      <main className={`${darkMode ? 'dark' : 'light'}`}>
       <Sidenav
         darkMode={darkMode}
         toggleDarkMode={toggleDarkMode}
-      />
-      <Home darkMode={darkMode} />
-      <About />
-      <Portfolio />
-      <Contact />
-      <Footer />
-    </main>
-  );
+        />
+        <div>
+          <Home darkMode={darkMode} />
+          <About />
+          <Portfolio />
+          <Contact />
+          <Footer />
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+          <h1>Vite + React</h1>
+          <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.jsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+        </div>
+      </main>
+    );
 }
 
-function SideNav() {
-  // component logic here
-
-  // return (
-  //   // JSX here
-  // )
-}
-
-export default App;
+export default App
